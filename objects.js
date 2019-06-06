110d252defdca00af29ce2eaebb1ec56bcfcef14
enchanting-process-7328 <peterlin5301997@gmail.com>
2019-06-06 20:26:51 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,3 +1,11 @@
 var playlist = {
+  imagineDragons: "natural"
+};
 
+function updatePlaylist(playlist, artistName, songTitle) {
+  playlist[artistName] = songTitle;
+}
+
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist[artistName];
 }
